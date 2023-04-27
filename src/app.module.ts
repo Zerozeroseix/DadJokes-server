168864e9dbb612909f9346ad63dc4e99eac86236
Zerozeroseix <mulomedicus@hotmail.com>
2023-04-27 10:37:24 +0200

    Deleted unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.development.env' })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  const logger = new Logger
+  await app.listen(8000, () => {
+    logger.verbose(`Application is listening on port 8000`)
+  });
 }
 bootstrap();
